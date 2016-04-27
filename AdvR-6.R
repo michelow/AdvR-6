65d4f3813a3f6f017aecd6bbe932a7a2aa2daad7
Warren Michelow <michelow@users.noreply.github.com>
2016-04-27 14:28:51 -0700

    Initial commit
    Files auto-generated by RStudio for a new project.

---
+++ AdvR-6.R
@@ -0,0 +1,9 @@
+# Advanced R - 6 - Git Hub and Collaboration
+#
+
+# First we configure Git using the command shell
+
+# git config --global user.name "~~~"
+# git config --global user.email "!!"
+
+#
